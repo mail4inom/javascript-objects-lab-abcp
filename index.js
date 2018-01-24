296f5e09a574025a8cca6dd195e5896f7f754d1c
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-01-24 01:52:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value){
+return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key){
+  var clone = Object.assign({}, object, [key]);
+  delete clone[key];
+  return clone;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
+
+// var obj = { foo: 'bar' }
+//
+// var newObj = Object.assign({}, obj)
+//
+// newObj // { foo: 'bar' }
+//
+// delete newObj.foo // true
+//
+// newObj // {}
+//
+// obj // { foo: 'bar' }
+// ```
